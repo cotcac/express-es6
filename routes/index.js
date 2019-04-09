de86f2d527b0dd4046f6951a2d36db05d1789b47
user_dev <EmilyWong@Emilys-MacBook-Pro.local>
2019-04-09 18:45:32 +0700

    controller

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const Users = require('./User_class');
+const Users = require('../controllers/User_class');
 
 const userA = new Users();
 
-router.get('/insert', userA.insert)
+router.get('/', userA.insert)
 router.get('/single', userA.single)
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
 module.exports = router;
